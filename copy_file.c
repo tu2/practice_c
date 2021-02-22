ee88ef2a28b3b9f668b6bdcd4fbf9d15db9b2f21
tu2 <d.catarig@btinternet.com>
2021-02-22 20:02:21 +0000

    changed error messages

--- copy_file.c
+++ copy_file.c
@@ -13,20 +13,20 @@ int main (int argc, char *argv[])
     char ch;
 
     if (argc != 3){
-        printf("Usage: copy_file <source> <destination>\n");
-        exit(1);
+        fprintf(stderr, "Usage: copy_file <source> <destination>\n");
+        exit(EXIT_FAILURE);
     }
 
     /* open source file */
     if (( source = fopen(argv[1], "rb")) == NULL){
-        printf("Can't open source file!\n");
-        exit(1);
+        fprintf(stderr, "Can't open source file!\n");
+        exit(EXIT_FAILURE);
     }
 
     /* open destination file */
     if (( dest = fopen(argv[2], "wb")) == NULL){
-        printf("Can't open destination file!\n");
-        exit(1);
+        fprintf(stderr, "Can't open destination file!\n");
+        exit(EXIT_FAILURE);
     }
 
     /* copy form source to destination*/
@@ -34,30 +34,30 @@ int main (int argc, char *argv[])
     while (!feof(source)){
         ch = fgetc(source);
         if (ferror(source)){
-            printf("Error reading source file!\n");
-            exit(1);
+            fprintf(stderr, "Error reading source file!\n");
+            exit(EXIT_FAILURE);
         }
 
         if (!feof(source)) 
             fputc(ch, dest);
         if (ferror(dest)){
-            printf("Error writing destination file!\n");
-            exit(1);
+            fprintf(stderr, "Error writing destination file!\n");
+            exit(EXIT_FAILURE);
         }
     }
     
     /* close files */
     
     if (fclose(source) == EOF){
-        printf("Error closing source file!\n");
-        exit(1);
+        fprintf(stderr, "Error closing source file!\n");
+        exit(EXIT_FAILURE);
     }
 
     if (fclose(dest) == EOF){
-        printf("Error closing destination file!\n");
-        exit(1);
+        fprintf(stderr, "Error closing destination file!\n");
+        exit(EXIT_FAILURE);
     }
 
 
-    return 0;
+    return (EXIT_SUCCESS);
 }
