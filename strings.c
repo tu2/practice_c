45e0b434956a1ca994d4de88faf5c93d6d7102a4
tu2 <d.catarig@btinternet.com>
2021-05-14 20:45:45 +0100

    more work on strings

--- strings.c
+++ strings.c
@@ -1,5 +1,5 @@
 /* 
- *  Notes on strings with some examples (SOURCE: K.N. King C Programming a Modern Approach 2nd ed,  man pages and some online resources)
+ *  NOTES ON STRINGS with some examples (SOURCE: K.N. King C Programming a Modern Approach 2nd ed, man pages and some online resources)
  *   #string.h -> create, store, copy, concatenate, length
  *   
  *   - strlen -- find length of string
